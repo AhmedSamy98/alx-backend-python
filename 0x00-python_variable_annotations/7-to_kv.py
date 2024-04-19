d2d87356bfe1942e09982dde18e64310bca6cb5e
Cap: Ahmed Samy <ahmed1samy1998@gmail.com>
2024-04-19 05:03:07 +0200

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-"""
-    This module contains basic annotation practices
-    Author: Peter Ekwere
-"""
-from typing import Union, Tuple
+""" Complex types - string and int/float to tuple"""
+from typing import Callable, Iterator, Union, Optional, List, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ ...
-
-    Args:
-        k (str): this is a string variable
-        v (Union[int, float]): this variable can be integer or float
-
-    Returns:
-        Tuple[str, float]: this tuple contains a string and a float
     """
-    return (k, v ** 2)
+    takes a string k and an int OR float v as arguments
+    returns a tuple.
+    """
+
+    return (k, v**2)
