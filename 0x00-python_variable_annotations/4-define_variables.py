bbe6f46dfc1f6a2eb3e45eb8936b33abf0de631c
Cap: Ahmed Samy <ahmed1samy1998@gmail.com>
2024-04-19 05:01:08 +0200

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" Define variables """
+"""
+    This Module contains basic type annotations practices
+    Author: Peter Ekwere
+"""
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = "Holberton"
+school: str = "Holberton" 
