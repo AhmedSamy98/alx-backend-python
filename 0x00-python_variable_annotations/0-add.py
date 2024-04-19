6e5f68e3b352c0fb0bbafaaddac69d5faf3e81d7
Cap: Ahmed Samy <ahmed1samy1998@gmail.com>
2024-04-19 05:09:09 +0200

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
-"""
-    This Module contains contains a simple annotation practice
-    Author: Peter
-"""
+
+'''
+    Basic annotations for variables.
+'''
 
 
 def add(a: float, b: float) -> float:
-    """ This function adds to floats and return a float result
-
-    Args:
-        a (float): This is the first Number
-        b (float): This is the second number
-
-    Returns:
-        float: This is the result from adding num1 and num 2
-    """
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
-
-
-if __name__ ==  "__main__":
-    pass
