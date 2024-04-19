95b04e9762e3cd9072881efea92693fb6fbf00f5
Cap: Ahmed Samy <ahmed1samy1998@gmail.com>
2024-04-19 05:01:47 +0200

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
-"""
-    This module contains basic annotations practices
-    Author: Peter Ekwere 
-"""
-from typing import List, Union
+
+""" mixed list """
+from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """ THis function takes a list mxd_lst of integers and floats and returns their sum as a float.
-
-    Args:
-        mxd_lst (List[Union[int, float]]): a list of ints and floats
-
-    Returns:
-        float: a float number
-    """
-    return sum(mxd_lst)
+    """  returns sum as a float. """
+    return float(sum(mxd_lst))
